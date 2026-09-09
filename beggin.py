8e834e4ee1278330d7ea89d6c403040cd59ba16c
JathurshanG <j.gnanaseelan@actongroup.com>
2026-09-10 00:45:28 +0200

    wip

--- beggin.py
+++ beggin.py
@@ -1,1 +1,13 @@
 import requests
+
+url = "https://www.sephora.com/sitemaps/products-sitemap.xml"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers, timeout=30)
+
+print(response.status_code)
+print(response.url)
+response.text
